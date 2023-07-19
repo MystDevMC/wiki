52c340e9c7b7ac846c17547c4fe805640f3b5ec2
MystDevMC <mystdevmc@gmail.com>
2023-07-19 11:58:54 +0700

    Try to fix that same error with another prebuild script

--- prebuild.js
+++ prebuild.js
@@ -3,21 +3,22 @@ const path = require("node:path");
 const fs = require("fs");
 const baseDir = process.cwd();
 
-const prebuildScripts = async () => {
-  const file = path.join(
-    baseDir,
-    "/node_modules",
-    "next/dist/server/require-hook.js"
-  );
 
-  const content = await fs.promises.readFile(file, "utf-8");
-  await fs.promises.writeFile(
-    file,
-    content.replace(
-      "if (process.env.__NEXT_PRIVATE_PREBUNDLED_REACT) {",
-      "if (true) {"
-    )
-  );
-};
+const prebuildScripts = async () => {
+    const file = path.join(
+        baseDir,
+        "/node_modules",
+        "@netlify/plugin-nextjs/lib/templates/requireHooks.js"
+    );
+
+    const content = await fs.promises.readFile(file, "utf-8");
+    await fs.promises.writeFile(
+        file,
+        content.replace(
+            "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'default';",
+            "const reactMode = process.env.__NEXT_PRIVATE_PREBUNDLED_REACT || 'next';"
+        )
+    );
+}
 
 prebuildScripts();
